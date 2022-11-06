3d6a95cf08ac4d32fafb733812af140f405494ee
turtle85917 <siu85917@gmail.com>
2022-11-07 08:31:36 +0900

    Decorating the date

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
+enum ANSI {
+  NORMAL,
+  BOLD,
+  ITALIC = 3,
+  UNDERLINE,
+  F_BLACK = 30,
+  F_RED,
+  F_GREEN,
+  F_YELLOW,
+  F_BLUE,
+  F_MAGENTA,
+  F_CYAN,
+  F_WHITE,
+  B_BLACK = 40,
+  B_RED,
+  B_GREEN,
+  B_YELLOW,
+  B_BLUE,
+  B_MAGENTA,
+  B_CYAN,
+  B_WHITE,
+}
+
 const esc = "";
 
-console.log(`${esc}[36;4mHello!${esc}[0m`);
+const date = new Date();
+
+console.log(`${esc}[4;34m[${new Intl.DateTimeFormat("kr", { dateStyle: "full", timeStyle: "medium" }).format(date)}]${esc}[0m SUCCESS`);
